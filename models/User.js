74fc6b19e3d65c246aa884788fc213d2bd48c2f6
UtkarshMudgal2802droid <utkarsh.mudgal123si@gmail.com>
2025-06-21 17:05:30 +0530

    Updated User.js

--- models/User.js
+++ models/User.js
@@ -2,19 +2,19 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
   password: { type: String, required: true }
 });
 
 // Hash password before saving user
-UserSchema.pre('save', async function(next) {
+UserSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
   this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 
 // Password match method
-UserSchema.methods.matchPassword = function(enteredPassword) {
+UserSchema.methods.matchPassword = function (enteredPassword) {
   return bcrypt.compare(enteredPassword, this.password);
 };
 
