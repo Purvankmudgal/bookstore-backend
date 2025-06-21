05216dbbed0e645c555e47869e19233b87483f03
Purvankmudgal <purvank.mudgal0807@gmail.com>
2025-06-21 21:02:56 +0530

    Added Categories in Book

--- models/Book.js
+++ models/Book.js
@@ -3,6 +3,12 @@ const mongoose = require('mongoose');
 const bookSchema = new mongoose.Schema({
   title: { type: String, required: true },
   author: { type: String, required: true },
-});
+  genre: { type: String },
+  publishedYear: { type: Number },
+  description: { type: String },
+  price: { type: Number, required: true },
+  stock: { type: Number, default: 0 },
+  coverImageUrl: { type: String } // Optional: For displaying book covers
+}, { timestamps: true });
 
 module.exports = mongoose.model('Book', bookSchema);
